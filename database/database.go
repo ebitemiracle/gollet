227f6276d54846adf199f451e63e38d442ad9800
ebitemiracle <ebitemiracle@gmail.com>
2024-05-20 14:00:05 +0100

    Updated the codebase from MySQL to Postgres

--- database/database.go
+++ database/database.go
@@ -1,15 +1,15 @@
 package database
 
 import (
-
+	"context"
 	"fmt"
 	"os"
-	"database/sql"
 
 	"github.com/joho/godotenv"
+	"github.com/jackc/pgx/v4"
 )
 
-func MySQLConnect() (*sql.DB, error) {
+func PostgreSQLConnect() (*pgx.Conn, error) {
 	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
@@ -23,19 +23,62 @@ func MySQLConnect() (*sql.DB, error) {
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	// Establish connection to MySQL database
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
-	db, err := sql.Open("mysql", dataSourceName)
+	// Establish connection to PostgreSQL database
+	dataSourceName := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
+	conn, err := pgx.Connect(context.Background(), dataSourceName)
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 
 	// Ping database
-	err = db.Ping()
+	err = conn.Ping(context.Background())
 	if err != nil {
 		return nil, fmt.Errorf("error pinging database: %w", err)
 	}
 
-	fmt.Println("Connected to MySQL database!")
-	return db, nil
+	fmt.Println("Connected to PostgreSQL database!")
+	return conn, nil
 }
+
+
+// package database
+
+// import (
+
+// 	"fmt"
+// 	"os"
+// 	"database/sql"
+
+// 	"github.com/joho/godotenv"
+// )
+
+// func MySQLConnect() (*sql.DB, error) {
+// 	// Load environment variables from .env file
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error loading .env file: %w", err)
+// 	}
+
+// 	// Get database connection details from environment variables
+// 	dbUsername := os.Getenv("DB_USERNAME")
+// 	dbPassword := os.Getenv("DB_PASSWORD")
+// 	dbHost := os.Getenv("DB_HOST")
+// 	dbPort := os.Getenv("DB_PORT")
+// 	dbName := os.Getenv("DB_NAME")
+
+// 	// Establish connection to MySQL database
+// 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
+// 	db, err := sql.Open("mysql", dataSourceName)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error connecting to database: %w", err)
+// 	}
+
+// 	// Ping database
+// 	err = db.Ping()
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error pinging database: %w", err)
+// 	}
+
+// 	fmt.Println("Connected to MySQL database!")
+// 	return db, nil
+// }
--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"go_code/pkg/user"
 	"go_code/pkg/auth"
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/v4/stdlib" // Import the PostgreSQL driver
 )
 
 func main() {
-
-	// APIs
+	// Initialize the Gin router
 	application := gin.Default()
 
+	// Define API endpoints and their handlers
 	application.POST("/user", user.UserRegistration)
 	application.GET("/user/:user_id", user.FetchSingleUser)
 	
@@ -19,5 +19,32 @@ func main() {
 	application.POST("/auth/reset_password", auth.UserResetPassword)
 	application.POST("/auth/forgot_password", auth.ForgotPassword)
 	application.POST("/auth/verify_pin", auth.VerifyPin)
+
+	// Run the application on port 8081
 	application.Run(":8081")
 }
+
+
+// package main
+
+// import (
+// 	"github.com/gin-gonic/gin"
+// 	"go_code/pkg/user"
+// 	"go_code/pkg/auth"
+// 	_ "github.com/go-sql-driver/mysql"
+// )
+
+// func main() {
+
+// 	// APIs
+// 	application := gin.Default()
+
+// 	application.POST("/user", user.UserRegistration)
+// 	application.GET("/user/:user_id", user.FetchSingleUser)
+	
+// 	application.POST("/auth/login", auth.UserLogin)
+// 	application.POST("/auth/reset_password", auth.UserResetPassword)
+// 	application.POST("/auth/forgot_password", auth.ForgotPassword)
+// 	application.POST("/auth/verify_pin", auth.VerifyPin)
+// 	application.Run(":8081")
+// }
